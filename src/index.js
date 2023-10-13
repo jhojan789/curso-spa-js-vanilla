d07c14ff06d5c781f1f505e2d61e4c5e0473b95f
Jhojan Tobón <jhojantobonm@gmail.com>
2023-10-13 12:42:01 -0500

    Created and added configuration for development HMR

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import router from '@routes/index';
+import './styles/styles.css'
 
 window.addEventListener('load', router); 
 
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 
 module.exports= {
@@ -10,7 +9,7 @@ module.exports= {
     filename: 'bundle.js',
 
   },
-  mode: 'production',
+  mode: 'development',
   resolve: {
     extensions: ['.js'],
     alias:{
@@ -42,17 +41,9 @@ module.exports= {
       inject: true,
       template: './public/index.html',
       filename: 'index.html'
-    }),
-
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: './src/styles/styles.css',
-          to: ''
-        }
-      ]
     })
-  
-  ]
 
+
+    
+  ],
 }
